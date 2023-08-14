ab0ffbc3cc201463a5b1959398a6219b8b5df967
thephez <thephez@users.noreply.github.com>
2023-08-14 19:08:35 +0000

    script: fix another reference link

--- scripts/2-readme-link-conversion-from-backup.sh
+++ scripts/2-readme-link-conversion-from-backup.sh
@@ -9,6 +9,7 @@ find . -iname "*.md" -exec sed -i 's~](reference-dapi-endpoints-platform-endpoin
 
 
 # Reference
+find . -iname "*.md" -exec sed -i 's~](reference-dapi-endpoints~](\.\./reference/dapi-endpoints.md~g' {} +
 
 # Platform Protocol Reference
 find . -iname "*.md" -exec sed -i 's~](platform-protocol-reference-data-contract#~](\.\./protocol-ref/data-contract.md#~g' {} +
