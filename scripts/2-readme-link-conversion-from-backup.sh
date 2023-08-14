82a68b6f25067bafaca5d8920a04ed28e953b79c
thephez <thephez@users.noreply.github.com>
2023-08-14 14:08:57 -0400

    script: add sections and example containing page anchor

--- scripts/2-readme-link-conversion-from-backup.sh
+++ scripts/2-readme-link-conversion-from-backup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Introduction
+
+# Tutorials
+
 # Explanations
 find . -iname "*.md" -exec sed -i 's~](reference-dapi-endpoints-platform-endpoints~](\.\./reference/dapi-endpoints-platform-endpoints.md~g' {} +
+
+
+# Reference
+
+# Platform Protocol Reference
+find . -iname "*.md" -exec sed -i 's~](platform-protocol-reference-data-contract#~](\.\./protocol-ref/data-contract.md#~g' {} +
+
+# Resources
 
